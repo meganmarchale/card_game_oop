b00841b2740b38bc1852242274cb9df31cafeef9
meganmarchale <megan.marchale@gmail.com>
2025-06-10 14:58:10 +0200

    files cleaning

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+What we want is just that a deck of cards gets created, and split between the different players and each player plays a card at every turn, until there are no cards left.
+
+- The game is working until each player doesn't have any cards left.
+- The game generates all the cards.
+- The game distributes all the cards.
+"""
+
+
+class 
+
+
+
+#player(deck/n_players)
