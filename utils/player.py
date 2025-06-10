0a69726b4defd9841c67c80f4f9a798e11cd050c
meganmarchale <megan.marchale@gmail.com>
2025-06-10 16:48:29 +0200

    cleaned

--- utils/player.py
+++ utils/player.py
@@ -1,19 +1,3 @@
-"""
-In player.py create a class Player that contains these attributes:
-
-cards which is a list of Card. (you will need to import Card from card.py). In a real card game, this is equivalent to the cards that the player has in his hands.
-turn_count which is an int starting a 0.
-number_of_cards which is an int starting at 0.
-history which is a list of Card that will contain all the cards played by the player.
-And some methods:
-
-play() that will:
-randomly pick a Card in cards.
-Add the Card to the Player's history.
-Print: {PLAYER_NAME} {TURN_COUNT} played: {CARD_NUMBER} {CARD_SYMBOL_ICON}.
-Return the Card.
-"""
-
 from card import Deck, Card
 import math
 import random
